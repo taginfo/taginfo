9956130b31e5377763361cca07b1653dee59ceef
Jochen Topf <jochen@topf.org>
2010-11-05 21:42:32 +0100

    Move search into its own file

---
+++ web/lib/ui/search.rb
@@ -0,0 +1,23 @@
+# web/lib/ui/search.rb
+class Taginfo < Sinatra::Base
+
+    get '/search' do
+        @title = 'Search results'
+        @breadcrumbs << @title
+
+        @escaped_search_string = escape_html(params[:search])
+
+        @key = @db.select('SELECT key FROM keys').
+            condition('key = ?', params[:search]).
+            get_first_value()
+
+        @substring_keys = @db.select('SELECT key FROM keys').
+            condition("key LIKE '%' || ? || '%' AND key != ?", params[:search], params[:search]).
+            order_by([:key], :key, 'ASC').
+            execute().
+            map{ |row| row['key'] }
+
+        erb :search
+    end
+
+end
--- web/taginfo.rb
+++ web/taginfo.rb
@@ -232,31 +232,11 @@ class Taginfo < Sinatra::Base
 
     #--------------------------------------------------------------------------
 
-    get '/search/?' do
-        @title = 'Search results'
-        @breadcrumbs << @title
-
-        @escaped_search_string = escape_html(params[:search])
-
-        @key = @db.select('SELECT key FROM keys').
-            condition('key = ?', params[:search]).
-            get_first_value()
-
-        @substring_keys = @db.select('SELECT key FROM keys').
-            condition("key LIKE '%' || ? || '%' AND key != ?", params[:search], params[:search]).
-            order_by([:key], :key, 'ASC').
-            execute().
-            map{ |row| row['key'] }
-
-        erb :search
-    end
-
-    #--------------------------------------------------------------------------
-
     load 'lib/api/db.rb'
     load 'lib/api/wiki.rb'
     load 'lib/api/josm.rb'
     load 'lib/api/reports.rb'
+    load 'lib/ui/search.rb'
     load 'lib/ui/reports.rb'
     load 'lib/ui/sources/db.rb'
     load 'lib/ui/sources/wiki.rb'
