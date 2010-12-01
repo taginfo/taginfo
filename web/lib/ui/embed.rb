8ff120fa123ebe191a55e9a9303a4ffcf67bccbe
Jochen Topf <jochen@topf.org>
2010-12-01 17:14:49 +0100

    New favicon; view for embedding into wiki improved

--- web/lib/ui/embed.rb
+++ web/lib/ui/embed.rb
@@ -3,14 +3,15 @@ class Taginfo < Sinatra::Base
 
     get '/embed/key' do
         @key = params[:key]
-        @res = @db.select("SELECT * FROM db.keys").condition('key = ?', @key).execute()[0]
+        @dbkey = @db.select("SELECT * FROM db.keys").condition('key = ?', @key).execute()[0]
         erb :'embed/key', :layout => :'embed/layout'
     end
 
     get '/embed/tag' do
         @key = params[:key]
         @value = params[:value]
-        @res = @db.select("SELECT * FROM db.tags").condition('key = ?', @key).condition('value = ?', @value).execute()[0]
+        @dbtag = @db.select("SELECT * FROM db.tags").condition('key = ?', @key).condition('value = ?', @value).execute()[0]
+        @dbkey = @db.select("SELECT * FROM db.keys").condition('key = ?', @key).execute()[0]
         erb :'embed/tag', :layout => :'embed/layout'
     end
 
