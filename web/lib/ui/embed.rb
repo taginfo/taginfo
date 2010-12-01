166884c66f98ceeb14666b62bfa3d75698676bd8
Jochen Topf <jochen@topf.org>
2010-12-01 16:16:20 +0100

    improved view for embedding into wiki

--- web/lib/ui/embed.rb
+++ web/lib/ui/embed.rb
@@ -3,15 +3,15 @@ class Taginfo < Sinatra::Base
 
     get '/embed/key' do
         @key = params[:key]
-        @count = @db.select("SELECT count_all FROM db.keys").condition('key = ?', @key).get_first_value().to_i
-        erb :'embed/key', :layout => false
+        @res = @db.select("SELECT * FROM db.keys").condition('key = ?', @key).execute()[0]
+        erb :'embed/key', :layout => :'embed/layout'
     end
 
     get '/embed/tag' do
         @key = params[:key]
         @value = params[:value]
-        @count = @db.select("SELECT count_all FROM db.tags").condition('key = ?', @key).condition('value = ?', @value).get_first_value().to_i
-        erb :'embed/tag', :layout => false
+        @res = @db.select("SELECT * FROM db.tags").condition('key = ?', @key).condition('value = ?', @value).execute()[0]
+        erb :'embed/tag', :layout => :'embed/layout'
     end
 
 end
