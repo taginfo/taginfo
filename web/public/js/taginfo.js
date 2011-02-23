8adbf45ce042413424bc24730ad8926aa88b2904
Jochen Topf <jochen@topf.org>
2011-02-23 17:44:02 +0100

    fix http://trac.openstreetmap.org/ticket/3537

--- web/public/js/taginfo.js
+++ web/public/js/taginfo.js
@@ -1,5 +1,17 @@
 // taginfo.js
 
+jQuery(document).ready(function() {
+    jQuery.getQueryString = (function(a) {
+        if (a == "") return {};
+        var b = {};
+        for (var i = 0; i < a.length; i++) {
+            var p=a[i].split('=');
+            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+        }
+        return b;
+    })(window.location.search.substr(1).split('&'))
+});
+
 function print_wiki_link(title, options) {
     if (title == '') {
         return '';
