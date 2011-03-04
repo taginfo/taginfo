7a68088378e21045c2ef13627adf5c87aec56fd0
Jochen Topf <jochen@topf.org>
2011-03-04 10:51:50 +0100

    update_all.sh can now download source databases

--- sources/update_all.sh
+++ sources/update_all.sh
@@ -1,11 +1,28 @@
 #!/bin/sh
+#------------------------------------------------------------------------------
 #
 #  Taginfo
 #
 #  update_all.sh DIR
 #
+#  Call this to update your Taginfo databases. All data will be store in the
+#  directory DIR. Create an empty directory before starting for the first time!
+#
+#  In this directory you will find:
+#  log      - directory with log files from running the update script
+#  download - directory with bzipped databases for download
+#  ...      - a directory for each source with database and possible some
+#             temporary files
+#
+#------------------------------------------------------------------------------
 
-SOURCES="josm potlatch merkaartor wiki db"
+# these sources will be downloaded from http://taginfo.openstreetmap.de/download/
+SOURCES_DOWNLOAD=""
+
+# these sources will be created from the actual sources
+SOURCES_CREATE="josm potlatch merkaartor wiki db"
+
+#------------------------------------------------------------------------------
 
 set -e
 
@@ -26,7 +43,16 @@ echo "`$DATECMD` Start update_all..."
 
 mkdir -p $DIR/download
 
-for source in $SOURCES; do
+for source in $SOURCES_DOWNLOAD; do
+    echo "====================================="
+    echo "Downloading $source..."
+    mkdir -p $DIR/$source
+    wget --quiet -O $DIR/download/taginfo-$source.db.bz2 http://taginfo.openstreetmap.de/download/taginfo-$source.db.bz2
+    bzcat $DIR/download/taginfo-$source.db.bz2 >$DIR/$source/taginfo-$source.db
+    echo "Done."
+done
+
+for source in $SOURCES_CREATE; do
     echo "====================================="
     echo "Running $source/update.sh..."
     mkdir -p $DIR/$source
@@ -42,7 +68,7 @@ cd master
 ./update.sh $DIR
 cd ..
 
-for source in $SOURCES; do
+for source in $SOURCES_CREATE; do
     echo "====================================="
     echo "Running bzip2 on $source..."
     bzip2 -9 -c $DIR/$source/taginfo-$source.db >$DIR/download/taginfo-$source.db.bz2
@@ -56,3 +82,5 @@ echo "Done."
 echo "====================================="
 echo "`$DATECMD` Done update_all."
 
+
+#-- THE END -------------------------------------------------------------------
