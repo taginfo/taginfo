ffad86b508e34f1b52527b1b541c112150611b8b
Jochen Topf <jochen@topf.org>
2011-04-22 18:40:10 +0200

    Improved build, new version of binary, READMEs

--- sources/db/update.sh
+++ sources/db/update.sh
@@ -2,18 +2,26 @@
 #
 #  Taginfo source: DB
 #
-#  update.sh DIR
+#  update.sh DIR [PLANETFILE]
 #
 
 set -e
 
+# uncomment this if you want to get a core file in case tagstats crashes
+#ulimit -c 1000000000
+
 DIR=$1
+PLANETFILE=$2
 
 DATECMD='date +%Y-%m-%dT%H:%M:%S'
 
 if [ "x" = "x$DIR" ]; then
-    echo "Usage: update.sh DIR"
+    echo "Usage: update.sh DIR [PLANETFILE]"
     exit 1
+fi
+
+if [ "x" = "x$PLANETFILE" ]; then
+    PLANETFILE=/osm/planet/var/current-planet.osm.pbf
 fi
 
 echo "`$DATECMD` Start db..."
@@ -32,7 +40,8 @@ sqlite3 $DATABASE <pre.sql
 echo "`$DATECMD` Running count... "
 HERE=`pwd`
 cd $DIR
-bzcat $DIR/planet.osm.bz2 | $HERE/osmium_tagstats -
+#valgrind --leak-check=full --show-reachable=yes $HERE/tagstats $PLANETFILE >valgrind.log 2>&1
+$HERE/tagstats $PLANETFILE
 cd $HERE
 
 echo "`$DATECMD` Running update_characters... "
