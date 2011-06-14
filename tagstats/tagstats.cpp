dacdbc5d4acd8a65d36a86c76a7433feedafecee
Jochen Topf <jochen@topf.org>
2011-06-14 16:03:49 +0200

    Changes in Osmium

--- tagstats/tagstats.cpp
+++ tagstats/tagstats.cpp
@@ -82,6 +82,7 @@ int main(int argc, char *argv[]) {
     }
 
     Osmium::OSMFile infile(argv[1]);
-    infile.read<MyTagStatsHandler>();
+    MyTagStatsHandler handler;
+    infile.read(handler);
 }
 
