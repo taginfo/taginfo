5fcd2160b9b7cf7ed8c9b261284ef13c90c1934b
Jochen Topf <jochen@topf.org>
2011-12-10 11:53:55 +0100

    generate list of interesting tags

---
+++ sources/master/interesting_tags.sql
@@ -0,0 +1,41 @@
+-- ============================================================================
+--
+--  Taginfo
+--
+--  interesting_tags.sql
+--
+-- ============================================================================
+
+.bail ON
+
+ATTACH DATABASE '__DIR__/db/taginfo-db.db'                 AS db;
+ATTACH DATABASE '__DIR__/wiki/taginfo-wiki.db'             AS wiki;
+ATTACH DATABASE '__DIR__/josm/taginfo-josm.db'             AS josm; 
+ATTACH DATABASE '__DIR__/potlatch/taginfo-potlatch.db'     AS potlatch; 
+ATTACH DATABASE '__DIR__/merkaartor/taginfo-merkaartor.db' AS merkaartor; 
+
+-- ============================================================================
+
+DROP TABLE IF EXISTS interesting_tags;
+CREATE TABLE interesting_tags (
+    key   TEXT,
+    value TEXT
+);
+
+INSERT INTO interesting_tags (key, value)
+    SELECT DISTINCT key, NULL FROM db.keys WHERE count_all > 10000
+    UNION
+    SELECT key, value FROM db.tags WHERE count_all > 10000;
+
+DELETE FROM interesting_tags WHERE key IN ('created_by', 'ele', 'height', 'is_in', 'layer', 'maxspeed', 'name', 'ref', 'width') AND value IS NOT NULL;
+DELETE FROM interesting_tags WHERE value IS NOT NULL AND key LIKE '%:%';
+DELETE FROM interesting_tags WHERE value IS NOT NULL AND key LIKE 'fresno_%';
+
+ANALYZE interesting_tags;
+
+.output __DIR__/interesting_tags.lst
+
+SELECT key FROM interesting_tags WHERE value IS NULL ORDER BY key;
+SELECT key || '=' || value FROM interesting_tags WHERE value IS NOT NULL ORDER BY key, value;
+
+-- ============================================================================
