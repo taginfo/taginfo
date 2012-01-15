9a667274f26d14105be5659d4b901e8f3840d896
Jochen Topf <jochen@topf.org>
2012-01-15 20:53:51 +0100

    Update copyright year

--- web/taginfo.rb
+++ web/taginfo.rb
@@ -9,7 +9,7 @@
 #
 #------------------------------------------------------------------------------
 #
-#  Copyright (C) 2010  Jochen Topf <jochen@remote.org>
+#  Copyright (C) 2012  Jochen Topf <jochen@remote.org>
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
