e9a029d76ae05981913f33e6b66ad7733d449a40
Jochen Topf <jochen@topf.org>
2012-08-20 19:32:52 +0200

    Cleanup license info

--- tagstats/string_store.hpp
+++ tagstats/string_store.hpp
@@ -1,6 +1,27 @@
 #ifndef STRING_STORE_HPP
 #define STRING_STORE_HPP
 
+/*
+
+  Copyright 2012 Jochen Topf <jochen@topf.org>.
+
+  This file is part of Tagstats.
+
+  Tagstats is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Tagstats is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Tagstats.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 #include <list>
 #include <stdexcept>
 #include <new>
