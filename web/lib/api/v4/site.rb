b14844558528208b17267d4761ec7655274e0e41
Jochen Topf <jochen@topf.org>
2013-01-07 09:34:12 +0100

    Move site API call to site/info and add site/sources

--- web/lib/api/v4/site.rb
+++ web/lib/api/v4/site.rb
@@ -1,7 +1,7 @@
 # web/lib/api/v4/site.rb
 class Taginfo < Sinatra::Base
 
-    api(4, 'site', {
+    api(4, 'site/info', {
         :description => 'Get information about this taginfo site.',
         :result => {
             :url         => :STRING,
@@ -18,6 +18,25 @@ class Taginfo < Sinatra::Base
             data[k] = TaginfoConfig.get("instance.#{k}") 
         end
         return data.to_json
+    end
+
+    api(4, 'site/sources', {
+        :description => 'Get information about the data sources used.',
+        :result => {
+            :name         => :STRING,
+            :data_until   => :STRING,
+            :update_start => :STRING,
+            :update_end   => :STRING
+        },
+        :example => { },
+        :ui => '/sources'
+    }) do
+        return Source.visible.map{ |source| {
+            :name         => source.name,
+            :data_until   => source.data_until,
+            :update_start => source.update_start,
+            :update_end   => source.update_end
+        }}.to_json
     end
 
 end
