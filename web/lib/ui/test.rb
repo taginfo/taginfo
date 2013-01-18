33936f3da93f8330a8af2116689e610fc50a38e2
Jochen Topf <jochen@topf.org>
2013-01-18 20:28:25 +0100

    Remove test/tags page

--- web/lib/ui/test.rb
+++ web/lib/ui/test.rb
@@ -6,18 +6,6 @@ class Taginfo < Sinatra::Base
         erb :'test/index'
     end
 
-    get! '/test/tags' do
-        section :test
-        @title = ['Tags', 'Test']
-        limit = 300;
-        (@min, @max) = @db.select('SELECT min(count) AS min, max(count) AS max FROM popular_keys').get_columns(:min, :max)
-        @tags = @db.select("SELECT key, count, (count - ?) / (? - ?) AS scale, in_wiki, in_josm FROM popular_keys ORDER BY count DESC LIMIT #{limit}", @min.to_f, @max, @min).
-            execute().
-            each_with_index{ |tag, idx| tag['pos'] = (limit - idx) / limit.to_f }.
-            sort_by{ |row| row['key'] }
-        erb :'test/tags'
-    end
-
     get '/test/wiki_import' do
         section :test
         @title = ['Wiki Import', 'Test']
