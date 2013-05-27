4b639d67d5975b3ae7c99400550f6eea6bd2bf2a
Jochen Topf <jochen@topf.org>
2013-05-27 19:45:02 +0200

    fix master update.sh

--- sources/master/update.sh
+++ sources/master/update.sh
@@ -39,8 +39,8 @@ min_count_tags=`../../bin/taginfo-config.rb sources.master.min_count_tags 10000`
 min_count_relations_per_type=`../../bin/taginfo-config.rb sources.master.min_count_relations_per_type 100`
 sqlite3 $DATABASE <languages.sql
 $EXEC_RUBY -pe "\$_.sub!(/__DIR__/, '$DIR')" master.sql | sqlite3 $DATABASE
-$EXEC_RUBY -pe "\$_.sub!(/__DIR__/, '$DIR').sub!(/__MIN_COUNT_TAGS__/, '$min_count_tags')" interesting_tags.sql | sqlite3 $DATABASE
-$EXEC_RUBY -pe "\$_.sub!(/__DIR__/, '$DIR').sub!(/__MIN_COUNT_RELATIONS_PER_TYPE__/, '$min_count_relations_per_type')" interesting_relation_types.sql | sqlite3 $DATABASE
+$EXEC_RUBY -pe "\$_.sub!(/__DIR__/, '$DIR')" interesting_tags.sql | $EXEC_RUBY -pe "\$_.sub!(/__MIN_COUNT_TAGS__/, '$min_count_tags')" | sqlite3 $DATABASE
+$EXEC_RUBY -pe "\$_.sub!(/__DIR__/, '$DIR')" interesting_relation_types.sql | $EXEC_RUBY -pe "\$_.sub!(/__MIN_COUNT_RELATIONS_PER_TYPE__/, '$min_count_relations_per_type')" | sqlite3 $DATABASE
 
 echo "`$DATECMD` Updating history database..."
 if [ ! -e $HISTORYDB ]; then
