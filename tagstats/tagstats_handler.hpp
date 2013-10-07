cc77f7a51174b7c1539dff00817128e8d68439d1
IrlJidel <donal.diamond@gmail.com>
2013-10-07 11:30:33 +0100

    use sources.master.min_count_tags to set min_tag_combination_count in tagstats_handler.hpp

--- tagstats/tagstats_handler.hpp
+++ tagstats/tagstats_handler.hpp
@@ -243,7 +243,7 @@ class TagStatsHandler : public Osmium::Handler::Base {
      * Tag combination not appearing at least this often are not written
      * to database.
      */
-    static const unsigned int min_tag_combination_count = 1000;
+    static const unsigned int min_tag_combination_count = TAGSTATS_MIN_COUNT_TAGS;
 
     time_t timer;
 
