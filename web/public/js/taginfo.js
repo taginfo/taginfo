e0887aca95abf7cda633573d66abea6cbc7ca73d
Jochen Topf <jochen@topf.org>
2014-05-19 10:08:55 +0200

    Disable comparison function if cookies are disabled in the browser.

--- web/public/js/taginfo.js
+++ web/public/js/taginfo.js
@@ -582,6 +582,16 @@ function open_help() {
 
 /* ============================ */
 
+function cookies_enabled() {
+    var cookieEnabled = (navigator.cookieEnabled) ? true : false;
+
+    if (typeof navigator.cookieEnabled == "undefined" && !cookieEnabled) {
+        document.cookie="testcookie";
+        cookieEnabled = (document.cookie.indexOf("testcookie") != -1) ? true : false;
+    }
+    return (cookieEnabled);
+}
+
 function get_comparison_list() {
     return jQuery.cookie('taginfo_comparison_list') || [];
 }
