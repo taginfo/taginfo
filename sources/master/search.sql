aeb9ff37d6683236300b0325d65c6ca0ec66c7ed
Kuang-che Wu <kcwu@csie.org>
2014-12-07 14:32:05 +0800

    make sqlite3 full-text search tokenizer configurable.
    OpenStreetMap is an international project. "icu" or other non-default
    tokenizer may be more suitable for some locales. For example, "icu"
    tokenizer is better than the default ("simple") for Chinese.
    
    We don't want to force all to recompile sqlite3 in order to use "icu"
    tokenizer, so make it configurable in taginfo-config.json.

--- sources/master/search.sql
+++ sources/master/search.sql
@@ -18,6 +18,7 @@ ATTACH DATABASE '__DIR__/db/taginfo-db.db' AS db;
 
 DROP TABLE IF EXISTS ftsearch;
 CREATE VIRTUAL TABLE ftsearch USING fts3 (
+    tokenize=__TOKENIZER__,
     key       TEXT,
     value     TEXT,
     count_all INTEGER
