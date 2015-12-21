54d3cd85f2a97529d120d16c9698bfa8cf757499
Jochen Topf <jochen@topf.org>
2015-12-21 20:19:25 +0100

    Use different separator character for nicer SQL output.

--- sources/db/show_selection_stats.sql
+++ sources/db/show_selection_stats.sql
@@ -1,5 +1,7 @@
 
-SELECT '  interesting_tags count ......... ', count(*) FROM interesting_tags;
-SELECT '  frequent_tags count ............ ', count(*) FROM frequent_tags;
-SELECT '  interesting_relation_types count ', count(*) FROM interesting_relation_types;
+.separator ': '
+
+SELECT '  interesting_tags count .........', count(*) FROM interesting_tags;
+SELECT '  frequent_tags count ............', count(*) FROM frequent_tags;
+SELECT '  interesting_relation_types count', count(*) FROM interesting_relation_types;
 
