3108663da106ec87dd0dace2240e609041a496da
Jochen Topf <jochen@topf.org>
2016-02-06 15:21:35 +0100

    Move hash helper functions into their own include file.

--- tagstats/tagstats_handler.hpp
+++ tagstats/tagstats_handler.hpp
@@ -35,45 +35,9 @@
 #include <osmium/util/memory.hpp>
 #include <osmium/util/verbose_output.hpp>
 
+#include "hash.hpp"
 #include "sqlite.hpp"
 #include "string_store.hpp"
-
-/**
- * Hash function used in google hash map that seems to work well with tag
- * key/value strings.
- */
-struct djb2_hash {
-    size_t operator()(const char *str) const {
-        size_t hash = 5381;
-        int c;
-
-        while ((c = *str++)) {
-            hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-        }
-
-        return hash;
-    }
-
-    size_t operator()(std::pair<const char *, const char*> p) const {
-        std::string s = p.first;
-        s += '=';
-        s += p.second;
-        return operator()(s.c_str());
-    }
-};
-
-/**
- * String comparison used in google hash map.
- */
-struct eqstr {
-    bool operator()(const char* s1, const char* s2) const {
-        return (s1 == s2) || (s1 && s2 && strcmp(s1, s2) == 0);
-    }
-
-    bool operator()(std::pair<const char*, const char*> p1, std::pair<const char*, const char*> p2) const {
-        return operator()(p1.first, p2.first) && operator()(p1.second, p2.second);
-    }
-};
 
 /**
  * Holds some counter for nodes, ways, and relations.
