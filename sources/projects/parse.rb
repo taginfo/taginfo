e88d0e33bcd0f4b5e45c24a35f521a2e7f176937
Jochen Topf <jochen@topf.org>
2016-08-08 09:49:18 +0200

    More thorough format check if project json files.

--- sources/projects/parse.rb
+++ sources/projects/parse.rb
@@ -81,7 +81,12 @@ end
 #------------------------------------------------------------------------------
 
 def parse_and_check(id, data, log, db)
-    if data[:data_format] != 1
+    unless data.is_a?(Hash)
+        log.fatal "JSON TOP-LEVEL IS NOT AN OBJECT."
+        return
+    end
+
+    unless data[:data_format].is_a?(Integer) && data[:data_format] == 1
         log.fatal "UNKNOWN OR MISSING data_format (KNOWN FORMATS: 1)."
         return
     end
@@ -89,10 +94,10 @@ def parse_and_check(id, data, log, db)
     db.execute("UPDATE projects SET data_format=?, data_url=? WHERE id=?", [data[:data_format], data[:data_url], id])
 
     if data[:data_updated]
-        if data[:data_updated].match(/^[0-9]{8}T[0-9]{6}Z$/)
+        if data[:data_updated].is_a?(String) && data[:data_updated].match(/^[0-9]{8}T[0-9]{6}Z$/)
             db.execute("UPDATE projects SET data_updated=? WHERE id=?", [data[:data_updated], id])
         else
-            log.error "project.data_updated MUST USE FORMAT 'yyyymmddThhmmssZ'. CURRENT VALUE IGNORED."
+            log.error "project.data_updated MUST USE FORMAT 'yyyymmddThhmmssZ'. CURRENT VALUE '#{data[:data_updated]}' IGNORED."
         end
     end
 
@@ -107,26 +112,39 @@ def parse_and_check(id, data, log, db)
         return
     end
 
+    unless data[:project].is_a?(Hash)
+        log.fatal "project MUST BE AN OBJECT."
+        return
+    end
+
     p = data[:project].clone
 
-    if ! p[:name]
-        log.fatal "MISSING project.name."
+    unless p[:name] && p[:name].is_a?(String)
+        log.fatal "MISSING project.name OR NOT A STRING."
     end
 
-    if ! p[:description]
-        log.fatal "MISSING project.description."
+    unless p[:description] && p[:name].is_a?(String)
+        log.fatal "MISSING project.description OR NOT A STRING."
     end
 
-    if ! p[:project_url]
-        log.fatal "MISSING project.project_url."
+    unless p[:project_url] && p[:name].is_a?(String)
+        log.fatal "MISSING project.project_url OR NOT A STRING."
     end
 
-    if ! p[:contact_name]
-        log.error "MISSING project.contact_name."
+    unless p[:contact_name] && p[:name].is_a?(String)
+        log.error "MISSING project.contact_name OR NOT A STRING."
     end
 
-    if ! p[:contact_email]
-        log.error "MISSING project.contact_email."
+    unless p[:contact_email] && p[:name].is_a?(String)
+        log.error "MISSING project.contact_email OR NOT A STRING."
+    end
+
+    if p[:doc_url] && !p[:doc_url].is_a?(String)
+        log.error "OPTIONAL project.doc_url MUST BE STRING."
+    end
+
+    if p[:icon_url] && !p[:icon_url].is_a?(String)
+        log.error "OPTIONAL project.icon_url MUST BE STRING."
     end
 
     db.execute("UPDATE projects SET name=?, description=?, project_url=?, doc_url=?, icon_url=?, contact_name=?, contact_email=? WHERE id=?", [
@@ -158,9 +176,16 @@ def parse_and_check(id, data, log, db)
         return
     end
 
+    unless data[:tags].is_a?(Array)
+        log.fatal "tags MUST BE AN ARRAY."
+        return
+    end
+
     data[:tags].each_with_index do |d, n|
         if d[:key].nil?
             log.error "MISSING tags.#{n}.key.\n"
+        elsif !d[:key].is_a?(String)
+            log.error "tags.#{n}.key MUST BE A STRING.\n"
         else
             on = { 'node' => 0, 'way' => 0, 'relation' => 0, 'area' => 0 }
             if d[:object_types]
@@ -183,6 +208,23 @@ def parse_and_check(id, data, log, db)
             else
                 on = { 'node' => 1, 'way' => 1, 'relation' => 1, 'area' => 1 }
             end
+
+            if d[:value] && !d[:value].is_a?(String)
+                log.error "OPTIONAL tag.X.value MUST BE STRING."
+            end
+
+            if d[:description] && !d[:description].is_a?(String)
+                log.error "OPTIONAL tag.X.description MUST BE STRING."
+            end
+
+            if d[:doc_url] && !d[:doc_url].is_a?(String)
+                log.error "OPTIONAL tag.X.doc_url MUST BE STRING."
+            end
+
+            if d[:icon_url] && !d[:icon_url].is_a?(String)
+                log.error "OPTIONAL tag.X.icon_url MUST BE STRING."
+            end
+
             db.execute("INSERT INTO project_tags (project_id, key, value, description, doc_url, icon_url, on_node, on_way, on_relation, on_area) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [
                 id,
                 d[:key],
