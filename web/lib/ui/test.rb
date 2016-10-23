487688cdfa8d84721eaa6ad19f41ab5ca95c8bc8
Jochen Topf <jochen@topf.org>
2016-10-23 21:14:51 +0200

    Remove now superfluous test page.

--- web/lib/ui/test.rb
+++ web/lib/ui/test.rb
@@ -6,11 +6,4 @@ class Taginfo < Sinatra::Base
         erb :'test/index'
     end
 
-    get '/test/wiki_import' do
-        section :test
-        @title = ['Wiki Import', 'Test']
-        @problems = @db.select('SELECT * FROM problems ORDER BY location, reason, lang, key, value').execute()
-        erb :'test/wiki_import'
-    end
-
 end
