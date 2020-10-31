e1599acace8ba56e17d234d72ddc66fa74cd60e3
Jochen Topf <jochen@topf.org>
2020-10-31 11:21:10 +0100

    Add some statistics to language and chronology sources

--- sources/chronology/post.sql
+++ sources/chronology/post.sql
@@ -1,9 +1,12 @@
 
-ANALYZE keys_chronology;
-
 CREATE INDEX keys_chronology_key ON keys_chronology (key);
 
-ANALYZE tags_chronology;
-
 CREATE INDEX tags_chronology_key_value ON tags_chronology (key, value);
+
+INSERT INTO stats (key, value)
+    SELECT 'chronology_num_keys', count(*) FROM keys_chronology;
+
+ANALYZE;
+
+UPDATE source SET update_end=datetime('now');
 
--- sources/languages/post.sql
+++ sources/languages/post.sql
@@ -4,6 +4,12 @@
 --  post.sql
 --
 
+INSERT INTO stats (key, value)
+    SELECT 'subtag_' || stype, count(*) FROM subtags GROUP BY stype;
+
+INSERT INTO stats (key, value)
+    SELECT 'wikipedia_sites', count(*) FROM wikipedia_sites;
+
 ANALYZE;
 
 UPDATE source SET update_end=datetime('now');
