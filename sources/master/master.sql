e7e0e8cc0c7c269f76f2702b93eb7c5714723fed
Jochen Topf <jochen@topf.org>
2022-05-31 11:01:06 +0200

    Optimize SQL: Getting tag counts for projects

--- sources/master/master.sql
+++ sources/master/master.sql
@@ -70,16 +70,12 @@ UPDATE projects.project_unique_tags SET in_wiki=0 WHERE in_wiki IS NULL;
 UPDATE projects.project_unique_keys SET count_all=(SELECT count_all FROM db.keys WHERE projects.project_unique_keys.key = db.keys.key);
 UPDATE projects.project_unique_keys SET count_all=0 WHERE count_all IS NULL;
 
-CREATE TEMP TABLE tags_count_all (
-    key       VARCHAR,
-    value     VARCHAR,
-    count_all INTEGER
-);
-
-INSERT INTO tags_count_all (key, value, count_all)
-    SELECT t.key, t.value, t.count_all FROM db.tags t, projects.project_unique_tags p WHERE t.key=p.key AND t.value = p.value;
-
+WITH tags_count_all AS
+    (SELECT t.key, t.value, t.count_all
+       FROM db.tags t, projects.project_unique_tags p
+       WHERE t.key = p.key AND t.value = p.value)
 UPDATE projects.project_unique_tags SET count_all=(SELECT count_all FROM tags_count_all WHERE projects.project_unique_tags.key = tags_count_all.key AND projects.project_unique_tags.value = tags_count_all.value);
+
 UPDATE projects.project_unique_tags SET count_all=0 WHERE count_all IS NULL;
 
 -- ============================================================================
