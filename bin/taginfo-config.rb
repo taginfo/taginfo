f58d373b9214f1b81ca2572e78ec57ace97e18ac
Jochen Topf <jochen@topf.org>
2022-10-24 09:46:21 +0200

    Fix taginfo-config.rb to use new TaginfoConfig interface

--- bin/taginfo-config.rb
+++ bin/taginfo-config.rb
@@ -7,9 +7,9 @@ require 'json'
 
 require File.expand_path(File.dirname(__FILE__)) + '/../web/lib/config.rb'
 
-TaginfoConfig.read
+taginfo_config = TaginfoConfig.new(File.expand_path(File.dirname(__FILE__)) + '/../../taginfo-config.json')
 
-value = TaginfoConfig.get(ARGV[0], ARGV[1])
+value = taginfo_config.get(ARGV[0], ARGV[1])
 if value.nil?
     puts ''
     exit 1
