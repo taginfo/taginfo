fbe25d7c537cc295f5871cc496441b5149971643
Jochen Topf <jochen@topf.org>
2023-07-01 16:55:05 +0200

    Add more stats to project source

--- sources/projects/post.sql
+++ sources/projects/post.sql
@@ -6,6 +6,9 @@
 
 INSERT INTO stats (key, value) SELECT 'projects', count(*) FROM projects;
 INSERT INTO stats (key, value) SELECT 'projects_ok', count(*) FROM projects WHERE status='OK';
+INSERT INTO stats (key, value) SELECT 'projects_fetch_error', count(*) FROM projects WHERE status='FETCH ERROR';
+INSERT INTO stats (key, value) SELECT 'projects_parse_error', count(*) FROM projects WHERE status='PARSE ERROR';
+INSERT INTO stats (key, value) SELECT 'projects_with_icon', count(*) FROM projects WHERE icon IS NOT NULL;
 INSERT INTO stats (key, value) SELECT 'project_keys', count(*) FROM project_tags WHERE value IS NULL;
 INSERT INTO stats (key, value) SELECT 'project_tags', count(*) FROM project_tags WHERE value IS NOT NULL;
 
@@ -21,6 +24,8 @@ CREATE TABLE project_unique_keys (
 INSERT INTO project_unique_keys (key, projects)
     SELECT key, count(*) FROM (SELECT DISTINCT key, project_id FROM project_tags) GROUP BY key;
 
+INSERT INTO stats (key, value) SELECT 'project_unique_keys', count(*) FROM project_unique_keys;
+
 CREATE TABLE project_unique_tags (
     key       VARCHAR NOT NULL,
     value     VARCHAR NOT NULL,
@@ -31,6 +36,8 @@ CREATE TABLE project_unique_tags (
 
 INSERT INTO project_unique_tags (key, value, projects)
     SELECT key, value, count(*) FROM (SELECT DISTINCT key, value, project_id FROM project_tags WHERE value IS NOT NULL) GROUP BY key, value;
+
+INSERT INTO stats (key, value) SELECT 'project_unique_tags', count(*) FROM project_unique_tags;
 
 CREATE INDEX project_unique_keys_key_idx ON project_unique_keys(key);
 
