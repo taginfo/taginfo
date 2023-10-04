7d245df1bfa9fcf09db98fcdb78f53cd4ff0cbc6
Jochen Topf <jochen@topf.org>
2023-10-04 17:10:56 +0200

    Fix: Don't create projects.project_unique_keys/tags any more

--- sources/projects/post.sql
+++ sources/projects/post.sql
@@ -14,37 +14,6 @@ INSERT INTO stats (key, value) SELECT 'project_tags', count(*) FROM project_tags
 
 -- ============================================================================
 
-CREATE TABLE project_unique_keys (
-    key       VARCHAR NOT NULL,
-    projects  INTEGER,
-    in_wiki   INTEGER,
-    count_all INTEGER
-);
-
-INSERT INTO project_unique_keys (key, projects)
-    SELECT key, count(*) FROM (SELECT DISTINCT key, project_id FROM project_tags) GROUP BY key;
-
--- INSERT INTO stats (key, value) SELECT 'project_unique_keys', count(*) FROM project_unique_keys;
-
-CREATE TABLE project_unique_tags (
-    key       VARCHAR NOT NULL,
-    value     VARCHAR NOT NULL,
-    projects  INTEGER,
-    in_wiki   INTEGER,
-    count_all INTEGER
-);
-
-INSERT INTO project_unique_tags (key, value, projects)
-    SELECT key, value, count(*) FROM (SELECT DISTINCT key, value, project_id FROM project_tags WHERE value IS NOT NULL) GROUP BY key, value;
-
--- INSERT INTO stats (key, value) SELECT 'project_unique_tags', count(*) FROM project_unique_tags;
-
-CREATE INDEX project_unique_keys_key_idx ON project_unique_keys(key);
-
-CREATE INDEX project_unique_tags_key_value_idx ON project_unique_tags(key, value);
-
--- ============================================================================
-
 UPDATE projects SET key_entries=(SELECT count(*) FROM project_tags WHERE project_id=id AND value IS NULL);
 UPDATE projects SET tag_entries=(SELECT count(*) FROM project_tags WHERE project_id=id AND value IS NOT NULL);
 
