9799fa857aa5efe1d8b7ca055a5e667420bb0727
Jochen Topf <jochen@topf.org>
2024-12-06 10:20:42 +0100

    Update copyright date

--- sources/languages/import_wikipedias.rb
+++ sources/languages/import_wikipedias.rb
@@ -7,7 +7,7 @@
 #
 #------------------------------------------------------------------------------
 #
-#  Copyright (C) 2020-2023  Jochen Topf <jochen@topf.org>
+#  Copyright (C) 2020-2024  Jochen Topf <jochen@topf.org>
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
